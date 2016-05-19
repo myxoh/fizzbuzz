a6571dc7cb7af69a590c29f72a2dd4f6b25e43b0
Nicolas Klein <myxoh10@gmail.com>
2016-05-20 00:05:37 +0100

    Some more formatting

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,12 @@
 class FavoritesController < ApplicationController
   def create
     Favorite.create favorite_params
-    redirect_to page_path params[:page]
+    redirect_to page_path(params[:page])
   end
 
   def destroy
     Favorite.find(params[:id]).destroy
-    redirect_to page_path params[:page]
+    redirect_to page_path(params[:page])
   end
   
   private
